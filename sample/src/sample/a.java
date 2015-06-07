03c56622cb29906f349fd25d65a1ceeae86d8ce0
s10528 <yunron1020@gmail.com>
2015-06-07 19:49:05 +0900

    サンプル～～(・ω・)

---
+++ sample/src/sample/a.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class a {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+		//確認用
+
+		System.out.println("本多");
+		//System.out.println("森");
+		//System.out.println("坂本");
+		//System.out.println("足立");
+		//System.out.println("木村");
+	}
+
+}
